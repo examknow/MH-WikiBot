d6433e90685b765328714cdbcdbbb393e2246413
examknow <examknow@xtremebnc.ml>
2020-03-27 14:28:22 -0500

    nasty prank to get locked user

--- bot.py
+++ bot.py
@@ -141,7 +141,7 @@ def on_message(bot, channel, sender, message):
                 GLOBALUSERINFO = DATA["query"]["globaluserinfo"]
                 for u in GLOBALUSERINFO:
                         try:
-                                if(len(str(u["locked"])) == 0):
+                            if str(u["locked"]) != 'foobar':
                                         bot.send_message(channel, str(u["name"]) + " (Globally Locked) has made " + str(u["editcount"]) + " edits on all Miraheze wikis")
                         except KeyError:
                                 bot.send_message(channel, str(u["name"]) + " has made " + str(u["editcount"]) + " on all Miraheze wikis")
