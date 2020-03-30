835b029ab19a5c7f7649e8b380c7d3652f4b4453
examknow <examknow@xtremebnc.ml>
2020-03-30 09:54:20 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -150,6 +150,66 @@ def on_message(bot, channel, sender, message):
             bot.send_message(channel, "An unexpected error has occured. I have reported this to Examknow.")
         except ValueError:  # includes simplejson.decoder.JSONDecodeError
             bot.send_message(channel, "An error occured. Did you type the wiki incorrectly? Does the user exist?") 
+    
+    if message.lower().startswith('!blockuser'):
+
+
+        S = requests.Session()
+
+        URL = "https://test.wikipedia.org/w/api.php"
+
+# Step 1: GET request to fetch login token
+        PARAMS_0 = {
+            "action": "query",
+            "meta": "tokens",
+            "type": "login",
+            "format": "json"
+        }
+
+        R = S.get(url=URL, params=PARAMS_0)
+        DATA = R.json()
+
+        LOGIN_TOKEN = DATA['query']['tokens']['logintoken']
+
+# Step 2: POST request to log in. Use of main account for login is not
+# supported. Obtain credentials via Special:BotPasswords
+# (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+        PARAMS_1 = {
+        "action": "login",
+        "lgname": "your_bot_username",
+        "lgpassword": "your_bot_password",
+        "lgtoken": LOGIN_TOKEN,
+        "format": "json"
+        }
+
+        R = S.post(URL, data=PARAMS_1)
+
+# Step 3: GET request to fetch CSRF token
+        PARAMS_2 = {
+            "action": "query",
+            "meta": "tokens",
+            "format": "json"
+        }
+
+        R = S.get(url=URL, params=PARAMS_2)
+        DATA = R.json()
+
+        CSRF_TOKEN = DATA['query']['tokens']['csrftoken']
+
+# Step 4: POST request to block user
+        PARAMS_3 = {
+            "action": "block",
+            "user": "Examknow",
+            "expiry": "infinite",
+            "reason": "Time out",
+            "token": CSRF_TOKEN,
+            "format": "json"
+        }
+
+        R = S.post(URL, data=PARAMS_3)
+        DATA = R.json()
+
+        bot.send_message(channel, DATA)
            
 def on_pm(bot, sender, message):
     global topic
