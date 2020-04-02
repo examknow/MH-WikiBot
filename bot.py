d2aa31103fc739529a835f5aefee2d878c6a4268
examknow <ek311@outlook.com>
2020-04-02 11:07:31 -0500

    fixing api param

--- bot.py
+++ bot.py
@@ -391,7 +391,7 @@ def on_message(bot, channel, sender, message):
             "summary": message + "(" + sender + ")",
             "appendtext": "\n* " + sender + ": " + message,
             "token": CSRF_TOKEN,
-            "bot": true,
+            "bot": "true",
             "format": "json"
         }
 
